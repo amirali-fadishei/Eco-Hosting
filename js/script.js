3f1146cc8b160f1da2ad96ed06418ec53da6dbda
amirali-fadishei <amirali83.fadishei@gmail.com>
2025-01-28 20:04:14 +0330

    Ai optimization

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 window.addEventListener("scroll", function () {
     var e = document.getElementById("totop");
-    window.pageYOffset > 1260 ? (e.classList.remove("d-none"), e.classList.add("d-flex")) : (e.classList.remove("d-flex"), e.classList.add("d-none"));
+    if (window.pageYOffset > 1260) {
+        e.classList.replace("d-none", "d-flex");
+    } else {
+        e.classList.replace("d-flex", "d-none");
+    }
 });
+
 var navbarToggler = document.getElementById("navbarToggler"),
     mobileMenu = document.getElementById("mobile-menu"),
     closeMenu = document.getElementById("close");
 
-    let navClick = false;
+let navClick = false;
 navbarToggler.addEventListener("click", function () {
-    if(navClick === false){
-        mobileMenu.style.right = "0px";
-        navClick = true;
-    }else{
-        mobileMenu.style.right = "-100%";
-        navClick = false;
-    }
-})
-var i,
-    accordian = document.getElementsByClassName("accordion");
-for (i = 0; i < accordian.length; i++)
-    accordian[i].addEventListener("click", function () {
+    mobileMenu.style.right = navClick ? "-100%" : "0px";
+    navClick = !navClick;
+});
+
+var accordian = document.getElementsByClassName("accordion");
+Array.from(accordian).forEach(function (element) {
+    element.addEventListener("click", function () {
         this.classList.toggle("accordian-active");
-        var i = this.nextElementSibling;
-        i.style.maxHeight ? (i.style.maxHeight = null) : (i.style.maxHeight = i.scrollHeight + "px");
+        var panel = this.nextElementSibling;
+        panel.style.maxHeight = panel.style.maxHeight ? null : panel.scrollHeight + "px";
     });
+});
