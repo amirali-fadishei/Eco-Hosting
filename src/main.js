861168f32f90f18f8b2f9a579011115b17fc4a0c
FadisheiWeb <amirali83.fadishei@gmail.com>
2025-08-04 22:55:26 +0330

    some performance issues fixed

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import './styles.scss';
 import * as bootstrap from 'bootstrap';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 import './style.css';
-
-AOS.init();
